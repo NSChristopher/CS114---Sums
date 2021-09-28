f4df18a05fbd1321384168f2650d98da8d9ebfdf
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-09-28 06:46:31 -0400

    Create Sums.java

---
+++ Sums.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sums {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String s = scnr.next();
+        Long n = scnr.nextLong();
+
+        System.out.println(Sums(s,n));
+
+    }
+    public static String Sums(String s, long n) {
+        return s;
+    }
+}
