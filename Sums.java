4644e40fb0bae3388fb691ea2301737db67dcdbc
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-09-29 14:56:17 -0400

    Finished Sums

--- Sums.java
+++ Sums.java
@@ -6,10 +6,21 @@ public class Sums {
         String s = scnr.next();
         Long n = scnr.nextLong();
 
-        System.out.println(Sums(s,n));
-
+        System.out.println(sums(s,n));
     }
-    public static String Sums(String s, long n) {
-        return s;
+
+    public static String sums(String s, long n) {
+        int length = s.length();
+        for(int i = 1; i < length; i++) {
+                Long left = Long.parseLong(s.substring(0,i));
+                Long right = Long.parseLong(s.substring(i));
+
+                if(left + right == n) 
+                        return left.toString() + "+" + right.toString();
+
+                if(left < n && !sums(right.toString(), n-left).equals("false"))
+                        return left.toString() + "+" + sums(right.toString(), n-left);
+        }
+        return "false";
     }
 }
